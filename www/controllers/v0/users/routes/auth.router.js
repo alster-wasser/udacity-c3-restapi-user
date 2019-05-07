b1278ebc460a204722e22deb271b6ba2963779ed
Sebastian Scheele <sebastian@loodse.com>
2019-05-07 09:12:50 +0200

    Update app

--- www/controllers/v0/users/routes/auth.router.js
+++ www/controllers/v0/users/routes/auth.router.js
@@ -17,6 +17,7 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const User_1 = require("../models/User");
+const c = __importStar(require("../../../../config/config"));
 const bcrypt = __importStar(require("bcrypt"));
 const jwt = __importStar(require("jsonwebtoken"));
 const EmailValidator = __importStar(require("email-validator"));
@@ -34,24 +35,25 @@ function comparePasswords(plainTextPassword, hash) {
     });
 }
 function generateJWT(user) {
-    return jwt.sign(user.short(), "hello");
+    console.log("generateJWT");
+    return jwt.sign(user.short(), c.config.jwt.secret);
 }
 function requireAuth(req, res, next) {
-    return next();
-    // if (!req.headers || !req.headers.authorization){
-    //     return res.status(401).send({ message: 'No authorization headers.' });
-    // }
-    // const token_bearer = req.headers.authorization.split(' ');
-    // if(token_bearer.length != 2){
-    //     return res.status(401).send({ message: 'Malformed token.' });
-    // }
-    // const token = token_bearer[1];
-    // return jwt.verify(token, "hello", (err, decoded) => {
-    //   if (err) {
-    //     return res.status(500).send({ auth: false, message: 'Failed to authenticate.' });
-    //   }
-    //   return next();
-    // });
+    //    return next();
+    if (!req.headers || !req.headers.authorization) {
+        return res.status(401).send({ message: 'No authorization headers.' });
+    }
+    const token_bearer = req.headers.authorization.split(' ');
+    if (token_bearer.length != 2) {
+        return res.status(401).send({ message: 'Malformed token.' });
+    }
+    const token = token_bearer[1];
+    return jwt.verify(token, c.config.jwt.secret, (err, decoded) => {
+        if (err) {
+            return res.status(500).send({ auth: false, message: 'Failed to authenticate.' });
+        }
+        return next();
+    });
 }
 exports.requireAuth = requireAuth;
 router.get('/verification', requireAuth, (req, res) => __awaiter(this, void 0, void 0, function* () {
